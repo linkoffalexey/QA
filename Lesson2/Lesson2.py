86a378150cc5920a4c50555a269a221110a05fa1
linkoffalexey <linkoffalexey@gmail.com>
2023-04-26 15:45:09 +0300

    Исправление

--- Lesson2/Lesson2.py
+++ Lesson2/Lesson2.py
@@ -2,6 +2,8 @@
 import time
 # Импортируем webdriver(набор команд для управления браузером)
 from selenium import webdriver
+
+
 # Вызываем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
 driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
 # Команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
@@ -12,7 +14,7 @@ time.sleep(5)
 # Метод find_element_by_xpath позволяет найти нужный элемент на сайте
 # Обсудим его чуть позже
 # Ищем элемент поля поиска
-textfield = driver.find_element_by_xpath("//input[@name='q']")
+textfield = driver.find_element_by_xpath('//textarea[@name="q"]')
 # Напишем текст в поле поиска
 textfield.send_keys("погода на завтра")
 time.sleep(5)
@@ -22,4 +24,4 @@ submit_button = driver.find_element_by_xpath("//div[@jsname='VlcLAe']//input[@cl
 submit_button.click() # После нажатия на кнопку должны появиться результаты поиска
 time.sleep(5)
 # Добавим команду, с помощью которой браузер будет сам закрываться после проведения теста
-driver.quit()
+#driver.quit()
